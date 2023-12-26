1581bd012c88c55e60d74731aa20543e6f18d7cd
cdsisthe1 <86140616+cdsisthe1@users.noreply.github.com>
2023-12-25 21:05:14 -0600

    Update background.js
    YouTube updated Fixed all add blocking

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ chrome.runtime.onInstalled.addListener((details) => {
   switch (details.reason) {
     case "install":
       console.info("EXTENSION INSTALLED");
+      // Reload all YouTube tabs on installation
       chrome.tabs.query({}, (tabs) => {
         tabs
           .filter((tab) => tab.url.startsWith("https://www.youtube.com/"))
@@ -23,39 +24,51 @@ chrome.runtime.onInstalled.addListener((details) => {
 });
 
 // Function to be injected into tabs
-function taimuRipu() {
-  const taimuRipuInner = async () => {
-    await new Promise((resolve, _reject) => {
+const taimuRipu = async () => {
+  try {
+    await new Promise((resolve) => {
       const videoContainer = document.getElementById("movie_player");
 
       const setTimeoutHandler = () => {
-        const isAd = videoContainer?.classList.contains("ad-interrupting") || videoContainer?.classList.contains("ad-showing");
-        const skipLock = document.querySelector(".ytp-ad-preview-text")?.innerText;
-        const surveyLock = document.querySelector(".ytp-ad-survey")?.length > 0;
+        try {
+          const isAd = videoContainer?.classList.contains("ad-interrupting") || videoContainer?.classList.contains("ad-showing");
+          const skipLock = document.querySelector(".ytp-ad-preview-text")?.innerText;
+          const surveyLock = document.querySelector(".ytp-ad-survey")?.length > 0;
 
-        if (isAd && skipLock) {
-          const videoPlayer = document.getElementsByClassName("video-stream")[0];
-          videoPlayer.muted = true;
-          videoPlayer.currentTime = videoPlayer.duration - 0.1;
-          if (videoPlayer.paused) videoPlayer.play();
-          document.querySelector(".ytp-ad-skip-button")?.click();
-          document.querySelector(".ytp-ad-skip-button-modern")?.click();
-        } else if (isAd && surveyLock) {
-          document.querySelector(".ytp-ad-skip-button")?.click();
-          document.querySelector(".ytp-ad-skip-button-modern")?.click();
+          console.log("Checking for ads...");
+
+          if (isAd && skipLock) {
+            console.log("Skipping skippable ad...");
+            const videoPlayer = document.getElementsByClassName("video-stream")[0];
+            videoPlayer.muted = true;
+            videoPlayer.currentTime = videoPlayer.duration - 0.1;
+            if (videoPlayer.paused) videoPlayer.play();
+            document.querySelector(".ytp-ad-skip-button")?.click();
+            document.querySelector(".ytp-ad-skip-button-modern")?.click();
+          } else if (isAd && surveyLock) {
+            console.log("Skipping survey ad...");
+            document.querySelector(".ytp-ad-skip-button")?.click();
+            document.querySelector(".ytp-ad-skip-button-modern")?.click();
+          } else {
+            console.log("No ads detected or ads are non-skippable.");
+          }
+
+          resolve();
+        } catch (error) {
+          console.error("Error during ad check:", error);
+          resolve(); // Resolve promise even on error
         }
-
-        resolve();
       };
 
+      // Run the check after a short delay
       setTimeout(setTimeoutHandler, 100);
     });
 
-    taimuRipuInner();
-  };
-
-  taimuRipuInner();
-}
+    taimuRipu(); // Recursive call for continuous execution
+  } catch (error) {
+    console.error("Error in taimuRipu function:", error);
+  }
+};
 
 // Listen for tab updates
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
@@ -63,6 +76,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     changeInfo.status === "complete" &&
     String(tab.url).includes("https://www.youtube.com/watch")
   ) {
+    console.log("YouTube video page loaded, executing taimuRipu...");
     chrome.scripting.executeScript({
       target: { tabId: tabId },
       function: taimuRipu,
